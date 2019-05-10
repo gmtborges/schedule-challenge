87ae9e48ad60aaaeb833c80d3c5b34d8fd91766b
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2019-05-09 21:28:01 -0300

    Add logo to login page

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Box, Heading } from 'grommet';
+import { Schedules } from 'grommet-icons';
 
 const LeftSide = () => (
 	<Box background="brand" width="50%" align="center">
 		<Heading margin={{ top: '330px' }}>Agendamento</Heading>
+		<Schedules size="xlarge" />
 	</Box>
 );
 
